007a20cd4655b56fd7d86aa4a722a48cfdf15272
Andrew Klotz <agc.klotz@gmail.com>
2017-06-08 23:13:40 -0400

    make and send commands

--- message_consumer/app/models/post.rb
+++ message_consumer/app/models/post.rb
@@ -1,4 +1,23 @@
 class Post < ApplicationRecord
   include MultipleMan::Subscriber
   subscribe fields: [:token]
+
+  def self.status
+    tokens = Post.all.pluck(:token).sort
+    {
+      total:     tokens.count,
+      duplicate: tokens.count - tokens.uniq.count,
+      missing:   missing(tokens)
+    }
+  end
+
+  def self.missing(tokens)
+    return 0 if tokens.empty?
+    (tokens.last - tokens.first + 1) - tokens.uniq.count
+  end
+
+  def self.clean
+    delete_all
+    status
+  end
 end
--- message_sender/app/models/post.rb
+++ message_sender/app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   include MultipleMan::Publisher
   publish fields: [:token]
+
+  def self.make(total)
+    total.times { |i| Post.create(token: i) }
+  end
+
+  def self.clean
+    delete_all
+  end
 end
